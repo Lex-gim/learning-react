da2976689d0845915d52a6210799dc6fac11ccd5
Miguel Ángel <vasquezmazo.miguelangel.112@gmail.com>
2024-11-14 17:33:56 -0500

    Mouse follower project

--- projects/02-mousefollower/src/App.jsx
+++ projects/02-mousefollower/src/App.jsx
@@ -1,6 +1,50 @@
-function App() {
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    console.log('efecto', {enabled});
+    const handleMove = (e) => {
+      const { clientX, clientY } = e
+      setPosition({x:clientX, y:clientY})
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
-    <h3>Proyecto 02</h3>
+  <>
+    <div style={{
+      display: `${enabled?'block':'none'}`,
+      position: 'absolute',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      border: '1px solid #fff',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+      transform: `translate(${position.x}px, ${position.y}px)`,
+    }}
+    />
+    <button onClick={()=>setEnabled(!enabled)}> {enabled?'Dejar':'Seguir'} mouse</button>
+  </>
+  )
+}
+
+function App() {
+    return (
+    <main>
+      <FollowMouse/>
+    </main>
   )
 }
 
